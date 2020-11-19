aecdda4fa8d20bd284eefd92d2a9e0be10e7da01
Tynan McAuley <tynan@galois.com>
2020-11-18 18:56:33 -0800

    Lock and buffer stdout for better performance

--- src/io.rs
+++ src/io.rs
@@ -1,16 +1,23 @@
-use std::io::{self, BufRead};
+use std::io::{self, BufRead, Write};
 
 use super::parser;
 
-pub fn process_streaming_input<S: BufRead>(stream: S) -> io::Result<()> {
-    for line in stream.lines() {
+pub fn process_streaming_input<I: BufRead, O: Write>(
+    istream: I,
+    ostream: &mut O,
+) -> io::Result<()> {
+    for line in istream.lines() {
         let line = line?;
         match parser::parse_value(&line) {
             // TODO: Disassemble `x`
-            Ok((end, (begin, x))) => println!("{}{:#x}{}", begin, x, end),
-            Err(_) => println!("{}", line),
+            Ok((end, (begin, x))) => writeln!(ostream, "{}{:#x}{}", begin, x, end)?,
+            Err(_) => writeln!(ostream, "{}", line)?,
         }
     }
+
+    // Technically not necessary, as `ostream` will flush when it's dropped, but this will
+    // explicitly show any errors when flushing, while flush-upon-drop masks flushing errors.
+    ostream.flush()?;
 
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
-use std::io;
+use std::io::{self, BufWriter};
 
 fn main() {
+    // Lock stdin and stdout to improve repeated read/write performance.
     let stdin = io::stdin();
     let locked_stdin = stdin.lock();
-    spike_dasm_rs::io::process_streaming_input(locked_stdin).unwrap();
+    let stdout = io::stdout();
+    let locked_stdout = stdout.lock();
+    let mut buffered_stdout = BufWriter::new(locked_stdout);
+
+    spike_dasm_rs::io::process_streaming_input(locked_stdin, &mut buffered_stdout).unwrap();
 }
