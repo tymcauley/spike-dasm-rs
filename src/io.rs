f55a095432b595b042eec216342aae3730abb52e
Tynan McAuley <tynan@galois.com>
2020-11-20 11:43:23 -0800

    Integrate disassembler into main binary
    You can now pipe a RocketChip trace into this crate's binary and see the
    disassembled output!

--- src/io.rs
+++ src/io.rs
@@ -1,16 +1,29 @@
 use std::io::{self, BufRead, Write};
 
+use super::disasm::Disassembler;
+use super::instruction::InstructionBits;
 use super::parser;
 
 pub fn process_streaming_input<I: BufRead, O: Write>(
     istream: I,
     ostream: &mut O,
+    disasm: Disassembler,
 ) -> io::Result<()> {
     for line in istream.lines() {
         let line = line?;
         match parser::parse_value(&line) {
-            // TODO: Disassemble `x`
-            Ok((end, (begin, x))) => writeln!(ostream, "{}{:#x}{}", begin, x, end)?,
+            Ok((end, (begin, x))) => {
+                let s = match InstructionBits::new(x) {
+                    Err(e) => format!("(error interpreting instruction: {})", e),
+                    Ok(inst_bits) => disasm
+                        .fmt_inst(inst_bits)
+                        .unwrap_or_else(|| "unknown".to_string()),
+                };
+                writeln!(ostream, "{}{}{}", begin, s, end)?;
+            }
+
+            // If the parse fails, that means this line doesn't contain anything to disassemble, so
+            // fall back to printing the original line.
             Err(_) => writeln!(ostream, "{}", line)?,
         }
     }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 use std::io::{self, BufWriter};
 
+use spike_dasm_rs::disasm::Disassembler;
+use spike_dasm_rs::instruction;
+use spike_dasm_rs::Xlen;
+
 fn main() {
+    let instructions = instruction::gen_instructions(Xlen::Rv64);
+    let disasm = Disassembler::new(instructions);
+
     // Lock stdin and stdout to improve repeated read/write performance.
     let stdin = io::stdin();
     let locked_stdin = stdin.lock();
@@ -8,5 +15,5 @@ fn main() {
     let locked_stdout = stdout.lock();
     let mut buffered_stdout = BufWriter::new(locked_stdout);
 
-    spike_dasm_rs::io::process_streaming_input(locked_stdin, &mut buffered_stdout).unwrap();
+    spike_dasm_rs::io::process_streaming_input(locked_stdin, &mut buffered_stdout, disasm).unwrap();
 }
